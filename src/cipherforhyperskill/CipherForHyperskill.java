497ec19ab0e202ba5fa7378c2b744304b2343334
anastasiya-belova <56701074+Kraz-1@users.noreply.github.com>
2019-11-21 02:24:57 +0300

    Revert "Modify the previous program to work with command-line arguments instead "
    This reverts commit 2c2e42ba3db1367bcfd3d1aa45aaf0f9d6a5d957.

--- src/cipherforhyperskill/CipherForHyperskill.java
+++ src/cipherforhyperskill/CipherForHyperskill.java
@@ -5,6 +5,9 @@
  */
 package cipherforhyperskill;
 
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
 /**
  *
  * @author Kraz
@@ -15,42 +18,15 @@ public class CipherForHyperskill {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String data = "";
-        boolean enc = true;
-        int key = 0;
-        int dataIndex = -1;
-        int modeIndex = -1;
-        int keyIndex = -1;
-        //поиск ключевых слов
-        for (int i = 0; i < args.length; i++){
-            if (args[i].equals("-data")){
-                dataIndex = i;
-            }
-            if (args[i].equals("-mode")){
-                modeIndex = i;
-            }
-            if (args[i].equals("-key")){
-                keyIndex = i;
-            }
+        Scanner sc = new Scanner(System.in);
+        String flag = sc.nextLine();
+        String str = sc.nextLine();
+        int key = sc.nextInt();
+        if (flag.equals("dec")){
+            System.out.println(decrypt(str, key));
         }
-        //добавление значений data, key, mode
-        if (dataIndex != -1){
-            data += args[dataIndex+1];
-        }
-        if (keyIndex != -1){
-            key = Integer.parseInt(args[keyIndex+1]);
-        }
-        if (modeIndex != -1){
-            if (args[modeIndex+1].equals("dec")){
-                enc = false;
-            }
-        }
-        //вызов функции
-        if(enc){
-            System.out.println(encrypt(data, key));
-        }
-        else{
-            System.out.println(decrypt(data, key));
+        if (flag.equals("enc")){
+            System.out.println(encrypt(str, key));
         }
     }
     
